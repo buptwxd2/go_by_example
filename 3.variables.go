df8c4dda862cc2cd7389b6ae501c5111292dfe93
xudong <xudong@xsky.com>
2020-10-15 20:22:37 +0800

    Add 5 examples

---
+++ 3.variables.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// omit the var type
+	var a = "initial"
+	fmt.Println(a)
+
+	// full typed
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	// declaration without initialization, zero-valued
+	var e int
+	fmt.Println(e)
+
+	// := is shot for delcaring and initializing
+	f := "apple"
+	fmt.Println(f)
+}
